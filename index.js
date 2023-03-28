173b8c46e566168a5ae47d091117c778dc3679e9
mohamedabdimohammud <mmohammud@outlook.com>
2023-03-28 10:43:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+ 
+  // Fetching the books data from the API
+fetch("https://anapioficeandfire.com/api/books")
+.then((resp) => resp.json())
+.then((books) => {
   
+  // Finding the 5th book in the series
+  const fifthBook = books[4];
+  console.log("The 5th book in the series is:", fifthBook.name);
+  
+  // Fetching the characters data from the API
+  fetch("https://anapioficeandfire.com/api/characters")
+    .then((resp) => resp.json())
+    .then((characters) => {
+      
+      // Finding the 1031st character in the series
+      const character1031 = characters[1030];
+      console.log("The 1031st character in the series is:", character1031.name);
+      
+      // Finding the total number of pages of all the books
+      const totalPages = books.reduce((total, book) => total + book.numberOfPages, 0);
+      console.log("The total number of pages of all the books is:", totalPages);
+    });
+});
+ 
 }
 
 function renderBooks(books) {
